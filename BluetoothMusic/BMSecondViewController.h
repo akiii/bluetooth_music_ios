e09068d097e717c3f4e5c9b0d303030fcfdf4033
akiii <akiii@sfc.wide.ad.jp>
2012-09-09 19:05:26 +0900

    Added a new view controller.

---
+++ BluetoothMusic/BMSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  BMSecondViewController.h
+//  BluetoothMusic
+//
+//  Created by Akifumi on 2012/09/09.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BMSecondViewController : UIViewController
+
+@end
---
+++ BluetoothMusic/BMSecondViewController.m
@@ -0,0 +1,43 @@
+//
+//  BMSecondViewController.m
+//  BluetoothMusic
+//
+//  Created by Akifumi on 2012/09/09.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import "BMSecondViewController.h"
+
+@interface BMSecondViewController ()
+
+@end
+
+@implementation BMSecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
