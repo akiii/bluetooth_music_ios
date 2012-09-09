fb4a4e2cbd89ae7a59093715c294f990e4b7dcd4
akiii <akiii@sfc.wide.ad.jp>
2012-09-09 17:58:56 +0900

    Modified a app delegate.

--- BluetoothMusic/AppDelegate.m
+++ BluetoothMusic/AppDelegate.m
@@ -7,12 +7,19 @@
 //
 
 #import "AppDelegate.h"
+#import "BMFirstViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    [self.window addSubview:[[BMFirstViewController alloc] initWithNibName:nil bundle:nil].view];
     return YES;
 }
 							
