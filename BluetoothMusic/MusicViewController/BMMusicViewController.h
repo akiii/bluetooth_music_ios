892fef4719a212f038594a6894c8fbcf120ef110
akiii <akiii@sfc.wide.ad.jp>
2012-09-14 06:35:26 +0900

    Added a new view controller.

---
+++ BluetoothMusic/MusicViewController/BMMusicViewController.h
@@ -0,0 +1,13 @@
+//
+//  BMMusicViewController.h
+//  BluetoothMusic
+//
+//  Created by Akifumi on 2012/09/14.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BMMusicViewController : UIViewController
+
+@end
---
+++ BluetoothMusic/MusicViewController/BMMusicViewController.m
@@ -0,0 +1,40 @@
+//
+//  BMMusicViewController.m
+//  BluetoothMusic
+//
+//  Created by Akifumi on 2012/09/14.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import "BMMusicViewController.h"
+
+@interface BMMusicViewController ()
+
+@end
+
+@implementation BMMusicViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        UIImage *tabBarItemImage = [UIImage imageNamed:@"music.png"];
+        self.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"music" image:tabBarItemImage tag:1];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+@end
